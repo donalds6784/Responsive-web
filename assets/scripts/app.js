4697acbe7b992ff0a3c9c9046aaf7780d870a5e5
Donalds6784 <donalds6784@gmail.com>
2020-02-03 03:23:00 +0300

    Made the Nav-bar responsive

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,16 @@
 'use strict';
 
-let responsiveNav;
+let responsiveNav, nav;
+responsiveNav = document.querySelector('.nav-collapse');
+nav = document.querySelector('.nav-modifier');
+
+responsiveNav.addEventListener('click', _ => {
+    if(nav.style.display === 'none') {
+        responsiveNav.classList.add('open');
+        nav.style.display = 'block';
+    } else {
+        responsiveNav.classList.remove('open');
+        nav.style.display = 'none';
+    }
+});
 
